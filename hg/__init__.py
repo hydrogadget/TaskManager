9b3473b20b8ee2f44b22240bb3b1aa2881914e91
Brian Munroe <brian.e.munroe@gmail.com>
2013-06-15 09:09:25 -0700

    making it run

--- hg/__init__.py
+++ hg/__init__.py
@@ -7,8 +7,8 @@ app.debug = True
 
 from hg.services import schedules
 from hg.utils.queues import SqliteQueue
-from hg.utils.stores import purge_backing_stores, get_priority_backing_store
-from hg.utils.stores import get_current_backing_store
+# from hg.utils.stores import purge_backing_stores, get_priority_backing_store
+# from hg.utils.stores import get_current_backing_store
 
 from hg.cron import water_scheduler_timer
 
@@ -17,7 +17,6 @@ s_view = schedules.API.as_view('schedule_api')
 app.add_url_rule('/schedules/<string:user_id>', view_func=s_view, methods=['GET',])
 app.add_url_rule('/schedules/', view_func=s_view, methods=['POST','DELETE'])
 
-purge_backing_stores()
-x = water_scheduler_timer()
-x.start()
+# purge_backing_stores()
+water_scheduler_timer().start()
 
