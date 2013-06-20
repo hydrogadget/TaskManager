ba83b42aa050397b09a8b1602cc3c3746c150149
Brian Munroe <brian.e.munroe@gmail.com>
2013-06-19 22:22:20 -0700

    making with the scheduling api, got the list working

--- hg/__init__.py
+++ hg/__init__.py
@@ -13,9 +13,8 @@ from hg.utils.stores import purge_backing_stores
 # API Routes for watering schedule
 #
 s_view = schedules.API.as_view('schedule_api')
-app.add_url_rule('/schedules/', view_func=s_view, methods=['POST','DELETE'])
-app.add_url_rule('/schedules/<string:user_id>', view_func=s_view, methods=['GET',])
-app.add_url_rule('/schedules/', view_func=s_view, methods=['POST','DELETE'])
+app.add_url_rule('/schedules/', view_func=s_view, methods=['GET','POST','DELETE'])
+app.add_url_rule('/schedules/<day>', view_func=s_view, methods=['GET'])
 
 l_view = location.API.as_view('location_api')
 app.add_url_rule('/location/', view_func=l_view, methods=['GET','POST','PUT','DELETE'])
--- hg/services/schedules.py
+++ hg/services/schedules.py
@@ -2,21 +2,36 @@ from flask.views import MethodView
 from flask import request, jsonify
 import time
 
-# from hg.utils.stores import get_current_backing_store, get_priority_backing_store
 from hg.utils.stores import get_hg_db
 
 class API(MethodView):
 
-    def get(self):
+    def get(self, day=None):
         db = get_hg_db()
-        cur = db.execute('select label, address, city, state, zip from location')
-        location = [dict(label=row[0], address=row[1], city=row[2], state=row[3], zip=row[4]) for row in cur.fetchall()]
-        return jsonify(location)
+
+        sql_stmt = 'select id, valve, duration, start_time, sun, mon, tue, wed, thu, fri, sat from schedule'
+
+        if (day != None):
+            sql_stmt = sql_stmt + ' where ' + day + ' = 1' 
+
+        schedules = []
+        for row in db.execute(sql_stmt):
+            schedules.append(dict(id=row[0],valve=row[1],duration=row[2],start_time=row[3],sun=row[4],mon=row[5],tue=row[6],wed=row[7],thu=row[8],fri=row[9],sat=row[10]))
+    
+        db.close()
+        return jsonify(schedules=schedules)
 
     def post(self):
         pass
 
+    def put(self):
+        pass
+
     def delete(self):
-        p = get_priority_backing_store()
-        j = p.popleft(sleep_wait=False)
-        return str(j) + " "
+        pass
+
+"""
+p = get_priority_backing_store()
+j = p.popleft(sleep_wait=False)
+return str(j) + " "
+"""
